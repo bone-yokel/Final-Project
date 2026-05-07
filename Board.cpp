4bf6664e7ba48d8952c22f4a5fcca0a9e44d6794
bone-yokel <midnitebones@gmail.com>
2026-05-06 21:42:27 -0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -8,3 +8,43 @@ Board::Board()
   opponent = 'O';
   multiplayer = false
 }
+
+void Board::changeTurn()
+{
+
+}
+
+void Board::showBoard();
+{
+
+}
+
+void Board::setMultiplayer(bool b);
+{
+
+}
+
+void Board::checkGameEnd();
+{
+
+}
+
+void Board::bestMove(char symbol);
+{
+
+}
+
+bool Board::isWinning(char symbol, int posistion);
+{
+
+}
+
+bool Board::isFork(char symbol, int posistion);
+{
+
+}
+
+void Board::makeMove(int position);
+{
+
+}
