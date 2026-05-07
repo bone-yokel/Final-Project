2b1df3b268b80e19a75d58beb11cf4cbc953d494
bone-yokel <midnitebones@gmail.com>
2026-05-06 21:04:10 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,1 +1,20 @@
+#pragma once
+#include <iostream>
 
+Class Board
+{
+private:
+  char current;
+  char opponent;
+  bool multiplayer;
+public:
+  Board();
+  void changeTurn();
+  void showBoard();
+  void setMultiplayer(bool);
+  void checkGameEnd();
+  void bestMove(char);
+  bool isWinning(char, int);
+  bool isFork(char, int);
+  void makeMove(int);
+};
