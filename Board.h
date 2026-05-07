71defd4e2a2bd778cfa1837d536bca26b8024ad8
bone-yokel <midnitebones@gmail.com>
2026-05-07 11:34:38 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -6,10 +6,16 @@ using namespace std;
 class Board
 {
 private:
-	char current;
+  char current;
   char opponent;
   bool multiplayer;
   char boardArray[9];
+  int wins[8][3] =
+    {
+        {0,1,2}, {3,4,5}, {6,7,8}, // rows
+        {0,3,6}, {1,4,7}, {2,5,8}, // columns
+        {0,4,8}, {2,4,6}           // diagonals
+    };
 public:
   Board();
   void changeTurn();
