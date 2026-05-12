1dbb3226db9a2244af7c652cbbe746a2012fa6c0
bone-yokel <midnitebones@gmail.com>
2026-05-12 10:16:53 -0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,10 +3,8 @@
 #include "Board.h"
 using namespace std;
 
-/*
-* Default constructor for Board Class
-* -Bones
-*/
+//constructor
+//Bones
 Board::Board()
 {
 	current = 'X';
@@ -15,33 +13,27 @@ Board::Board()
 	for (int x = 0; x < 9; x++) boardArray[x] = ' ';
 }
 
-/*
-* void function to get player count from user,
-* validates input
-* -Ross
-*/
+//gets player count
 void Board::playerCount()
 {
-    cout << "Will you be playing with another person? Y/N: ";
-    cin >> playerAnswer;
+	cout << "Will you be playing with another person? Y/N: ";
+	cin >> playerAnswer;
 
-    //Making sure input is valid
-    while (playerAnswer != 'Y' && playerAnswer != 'y' && playerAnswer != 'N' && playerAnswer != 'n')
-    {
-        cout << "Sorry, that is an invalid answer. Please enter either 'Y' or 'N'" << endl;
-            cout << "Will you be playing with another person? Y/N: ";
-        cin >> playerAnswer;
-    }
+	//Making sure input is valid
+	while (playerAnswer != 'Y' && playerAnswer != 'y' && playerAnswer != 'N' && playerAnswer != 'n')
+	{
+		cout << "Sorry, that is an invalid answer. Please enter either 'Y' or 'N'" << endl;
+		cout << "Will you be playing with another person? Y/N: ";
+		cin >> playerAnswer;
+	}
 
-    //setting multiplaer private member variable according to input
-    if (playerAnswer == 'Y' || playerAnswer == 'y') multiplayer = true;
-    else multiplayer = false;
+	//setting multiplaer private member variable according to input
+	if (playerAnswer == 'Y' || playerAnswer == 'y') multiplayer = true;
+	else multiplayer = false;
 }
 
-/*
-* void function that swaps the symbols of the two players to signify the end of their turn
-* -Srujan
-*/
+//Swaps the symbols of current and opponenet players
+//Srujan
 void Board::changeTurn()
 {
 	if (current == 'X')
@@ -56,34 +48,27 @@ void Board::changeTurn()
 	}
 }
 
-/*
-* void function that prints the current state of the board
-* -Bones
-*/
+//prints the current state of the board
+//Bones
 void Board::showBoard()
 {
 	cout << "0\t\t|1\t\t|2\n"; // numbering for the top row
 	cout << "\t" << boardArray[0] << "\t|\t" << boardArray[1] << "\t|\t" << boardArray[2] << "\t\n";
 	cout << "\t\t|\t\t|\n";
 	cout << "------------------------------------------------\n";
-	
+
 	cout << "3\t\t|4\t\t|5\n"; // numbering for the middle row
 	cout << "\t" << boardArray[3] << "\t|\t" << boardArray[4] << "\t|\t" << boardArray[5] << "\t\n";
 	cout << "\t\t|\t\t|\n";
 	cout << "------------------------------------------------\n";
-	
+
 	cout << "6\t\t|7\t\t|8\n"; // numbering for the bottom row
 	cout << "\t" << boardArray[6] << "\t|\t" << boardArray[7] << "\t|\t" << boardArray[8] << "\t\n";
 	cout << "\t\t|\t\t|\n";
 
-	
+
 }
 
-/*
-* void function that prints the result of the game
-* takes char result argument
-* -Ross
-*/
 void Board::gameEnd(char result)
 {
 	if (result == 'X') cout << endl << "X won the game!";
@@ -91,99 +76,102 @@ void Board::gameEnd(char result)
 	else cout << endl << "The game ended in a draw!";
 }
 
-/*
-* void function that checks the board to see if the game is over
-* -Srujan
-*/
-void Board::checkGameEnd()
+//Cheks if the game has ended and passes for who wins
+//Srujan
+bool Board::checkGameEnd()
 {
 	bool gameOver = false;
 	int fullTiles = 0;
 	for (int x = 0; x < 8 && gameOver == false; x++)
-    {
-        if (boardArray[wins[x][0]] == current &&
-            boardArray[wins[x][1]] == current &&
-            boardArray[wins[x][2]] == current  )
+	{
+		if (boardArray[wins[x][0]] == current &&
+			boardArray[wins[x][1]] == current &&
+			boardArray[wins[x][2]] == current)
 		{
-    	gameEnd(current);
-		gameOver = true;
+			gameEnd(current);
+			gameOver = true;
+			return gameOver;
 		}
-			
+
 		else if (boardArray[wins[x][0]] == opponent &&
-           		 boardArray[wins[x][1]] == opponent &&
-            	 boardArray[wins[x][2]] == opponent  )
+			boardArray[wins[x][1]] == opponent &&
+			boardArray[wins[x][2]] == opponent)
 		{
-		gameEnd(opponent);
-		gameOver = true;
+			gameEnd(opponent);
+			gameOver = true;
+			return gameOver;
 		}
-    }
+	}
 	if (!gameOver)
 	{
 		for (int x = 0; x <= 8; x++) if (boardArray[x] == 'X' || boardArray[x] == 'O') fullTiles++;
-		if (fullTiles == 9) gameEnd('T');
+		if (fullTiles == 9)
+		{
+			gameEnd('T');
+			return true;
+		}
+		else return false;
 		fullTiles = 0;
+	
 	}
 }
-
-/*
-* void function the finds and makes the best possible move, using a series of 7 steps, for the current player
-* -Bones
-*/
+//finds and makes the best possible move for the current player
+//Bones
 void Board::bestMove()
 {
 	bool moveFound = false;
-	
-    // 1. Take a winning move if possible
-    for (int x = 0; x < 9 && moveFound == false; x++)
-    {
-        if (boardArray[x] == ' ')
-        {
-            if (isWinning(current, x))
+
+	// 1. Take a winning move if possible
+	for (int x = 0; x < 9 && moveFound == false; x++)
+	{
+		if (boardArray[x] == ' ')
+		{
+			if (isWinning(current, x))
 			{
 				boardArray[x] = current;
 				moveFound = true;
 			}
-        }
-    }
+		}
+	}
 
 	// 2. Block opponents winning move
 	if (moveFound == false)
 	{
 		for (int x = 0; x < 9 && moveFound == false; x++)
-    	{
-        	if (boardArray[x] == ' ')
-       		{
-            	if (isWinning(opponent, x))
+		{
+			if (boardArray[x] == ' ')
+			{
+				if (isWinning(opponent, x))
 				{
 					boardArray[x] = current;
 					moveFound = true;
 				}
-        	}
-    	}
+			}
+		}
 	}
-	
+
 	// 3. Create a fork
 	if (moveFound == false)
 	{
 		int forkPosition = findFork(current);
-		if (forkPosition !=  000)
+		if (forkPosition != 000)
 		{
 			boardArray[forkPosition] = current;
 			moveFound = true;
 		}
 	}
-	
+
 	// 4. Block opponent fork
 	if (moveFound == false)
 	{
 		int forkPosition = findFork(opponent);
-		if (forkPosition !=  000)
+		if (forkPosition != 000)
 		{
 			boardArray[forkPosition] = current;
 			moveFound = true;
 		}
 	}
-	
+
 	// 5. Take center
 	if (moveFound == false)
 	{
@@ -193,33 +181,33 @@ void Board::bestMove()
 			moveFound = true;
 		}
 	}
-	
+
 	// 6. Take corner
 	if (moveFound == false)
 	{
-		int corners[4] = {0, 2, 6, 8};
-	    for (int x = 0; x < 4 && moveFound == false; x++)
-    	{
-       		if (boardArray[corners[x]] == ' ')
+		int corners[4] = { 0, 2, 6, 8 };
+		for (int x = 0; x < 4 && moveFound == false; x++)
+		{
+			if (boardArray[corners[x]] == ' ')
 			{
 				boardArray[corners[x]] = current;
 				moveFound = true;
 			}
-    	}
+		}
 	}
-	
+
 	// 7. Take edge
 	if (moveFound == false)
 	{
-	    int edges[4] = {1, 3, 5, 7};
-    	for (int x = 0; x < 4 && moveFound == false; x++)
-   		{
-        	if (boardArray[edges[x]] == ' ')
+		int edges[4] = { 1, 3, 5, 7 };
+		for (int x = 0; x < 4 && moveFound == false; x++)
+		{
+			if (boardArray[edges[x]] == ' ')
 			{
 				boardArray[edges[x]] = current;
 				moveFound = true;
 			}
-    	}
+		}
 	}
 }
 
@@ -228,13 +216,14 @@ void Board::bestMove()
 bool Board::isWinning(char symbol, int position) {
 	boardArray[position] = symbol;
 	for (int x = 0; x < 8; x++)
-    {
-        if (boardArray[wins[x][0]] == symbol &&
-            boardArray[wins[x][1]] == symbol &&
-            boardArray[wins[x][2]] == symbol  ) {
-		boardArray[position] = ' ';
-    	return true; }
-    }
+	{
+		if (boardArray[wins[x][0]] == symbol &&
+			boardArray[wins[x][1]] == symbol &&
+			boardArray[wins[x][2]] == symbol) {
+			boardArray[position] = ' ';
+			return true;
+		}
+	}
 	boardArray[position] = ' ';
 	return false;
 }
@@ -242,27 +231,27 @@ bool Board::isWinning(char symbol, int position) {
 //Bones
 int Board::findFork(char symbol)
 {
-    for (int i = 0; i < 9; i++)
-    {
-        if (boardArray[i] == ' ')
-        {
-            boardArray[i] = symbol;
-            int winningMoves = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		if (boardArray[i] == ' ')
+		{
+			boardArray[i] = symbol;
+			int winningMoves = 0;
 
-            for (int x = 0; x < 9; x++)
-            {
-                if (boardArray[x] == ' ')
-                {
-                    if (isWinning(symbol, x)) winningMoves++;
-                }
-            }
+			for (int x = 0; x < 9; x++)
+			{
+				if (boardArray[x] == ' ')
+				{
+					if (isWinning(symbol, x)) winningMoves++;
+				}
+			}
 
-            boardArray[i] = ' '; // resetting
+			boardArray[i] = ' '; // resetting
 
-            if (winningMoves >= 2) return i; // return after checking all x spots
-        }
-    }
-    return 000; // return -1 to indicate no fork being found
+			if (winningMoves >= 2) return i; // return after checking all x spots
+		}
+	}
+	return 000; // return -1 to indicate no fork being found
 }
 
 //Bones
