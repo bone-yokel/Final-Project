9c65a12dd066b372f829caf6e6ee4c9e57ff550f
bone-yokel <midnitebones@gmail.com>
2026-05-12 10:17:14 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -6,29 +6,29 @@ using namespace std;
 class Board
 {
 private:
-  char current;
-  char opponent;
-  bool multiplayer;
-  char playerAnswer;
-  char boardArray[9];
-  int wins[8][3] =
+    char current;
+    char opponent;
+    bool multiplayer;
+    char playerAnswer;
+    char boardArray[9];
+    int wins[8][3] =
     {
         {0,1,2}, {3,4,5}, {6,7,8}, // rows
         {0,3,6}, {1,4,7}, {2,5,8}, // columns
         {0,4,8}, {2,4,6}           // diagonals
     };
 public:
-  Board();
-  void resetboardArray(int x) {boardArray[x] = ' ';}
-  bool getMultiplayer() {return multiplayer;}
-  char getCurrent() {return current;}
-  void gameEnd(char result);
-  void changeTurn();
-  void showBoard();
-  void checkGameEnd();
-  void bestMove();
-  bool isWinning(char symbol, int posistion);
-  int findFork(char symbol);
-  void makeMove(int position);
-  void playerCount();
+    Board();
+    void resetboardArray(int x) { boardArray[x] = ' '; }
+    bool getMultiplayer() { return multiplayer; }
+    char getCurrent() { return current; }
+    void gameEnd(char result);
+    void changeTurn();
+    void showBoard();
+    bool checkGameEnd();
+    void bestMove();
+    bool isWinning(char symbol, int posistion);
+    int findFork(char symbol);
+    void makeMove(int position);
+    void playerCount();
 };
